b849f6480cd24fc6806165630cfe2e98d24f6390
Zakhar Rozental <rozental.zakhar@gmail.com>
2022-06-15 09:18:09 +0300

    Add text to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,9 +12,11 @@ const forecast = (latitude,longitude, callback) =>{
         }else{
               const originalDegree=body.current.temperature
               const feelsLike =body.current.feelslike
+              const windSpeed = body.current.wind_speed
+              const weatherDescription = body.current.weather_descriptions[0]
               
               callback(undefined, 
-                'The original tyemperature is: '+ originalDegree+' and it feels like: '+ feelsLike
+                'The weather is '+weatherDescription+'. The original tyemperature is: '+ originalDegree+' and it feels like: '+ feelsLike+ ' and the wind speed is: '+windSpeed
                 
             )
         }
